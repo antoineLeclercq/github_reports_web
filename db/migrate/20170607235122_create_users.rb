ed84f9c66d9075600bd010743f19b394909a54f2
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-06-07 20:06:34 -0400

    create user model and user migration

---
+++ db/migrate/20170607235122_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :provider, null: false
+      t.string :remote_id, null: false
+      t.string :name, null: false
+      t.string :token, null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170607235122) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider",  null: false
+    t.string "remote_id", null: false
+    t.string "name",      null: false
+    t.string "token",     null: false
+  end
 
 end
